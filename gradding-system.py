12d6d07799a7c4fd89ed0a0aa4c024ddb68ff2be
stevendzvukamanja4-cmd <stevendzvukamanja4@gmail.com>
2026-05-05 16:03:21 +0200

    Gradding-system

---
+++ gradding-system.py
@@ -0,0 +1,18 @@
+student_mark=80
+if 75<=student_mark<=100:
+    print("distinction")
+elif 60<= student_mark<=75:
+    print("pass")
+elif 50<=student_mark<=60:
+    print("credit")
+elif 35<= student_mark<=50:
+    print("almost")
+elif 20<= student_mark<=35:
+    print("fail")
+elif 0<= student_mark<=20:
+    print("ungraded")
+else:
+    print("enter the right mark")
+
+
+
